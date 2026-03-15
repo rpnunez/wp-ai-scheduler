f8ae0527e200eecad28d6d9f32c6984b282970d4
Raymond Nunez <rpnunez@gmail.com>
2026-03-15 02:28:15 -0400

    Delete fix_admin_js_empty_state.php

--- fix_admin_js_empty_state.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = 'ai-post-scheduler/assets/js/admin.js';
-$content = file_get_contents($file);
-if (strpos($content, "$('.aips-schedule-table').closest('.aips-content-panel');") !== false) {
-    echo "The code has been properly replaced.\n";
-} else {
-    echo "Failed to replace the code.\n";
-}
