24154fcb96222a9a98472b0c905f7f96799bf9fe
Raymond Nunez <rpnunez@gmail.com>
2026-03-18 17:37:13 -0400

    Delete fix_js.php

--- fix_js.php
+++
@@ -1,8 +0,0 @@
-<?php
-$content = file_get_contents('ai-post-scheduler/templates/admin/settings.php');
-$content = str_replace('<div id="tab-general" class="aips-tab-content aips-settings-tab-content active" style="display: block;">
-                        <?php do_settings_sections(\'aips-settings\'); ?>
-                    </div>', '<?php do_settings_sections(\'aips-settings\'); ?>', $content);
-
-file_put_contents('ai-post-scheduler/templates/admin/settings.php', $content);
-echo "Fixed wrapper.\n";
