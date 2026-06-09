85d453c871903adea01867cbaaae84b9adc5df3e
Raymond Nunez <rpnunez@gmail.com>
2026-06-08 22:59:20 -0400

    fix pr review comment

--- scripts/install-wp-tests.sh
+++ scripts/install-wp-tests.sh
@@ -20,9 +20,9 @@ WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
 download() {
-	if [ `which curl` ]; then
-		curl -s "$1" > "$2";
-	elif [ `which wget` ]; then
+	if command -v curl >/dev/null 2>&1; then
+		curl -s "$1" > "$2"
+	elif command -v wget >/dev/null 2>&1; then
 		wget -nv -O "$2" "$1"
 	fi
 }
